df870585dc81dfcb30a322041ab9ebfe15195d36
SiFlAMeSE <flame_uz@hotmail.com>
2019-01-01 16:20:29 +0700

    update V2 2019

--- src/pages/admin/report.js
+++ src/pages/admin/report.js
@@ -1,36 +1,76 @@
-import React, { Component } from 'react';
-import { Form, Input, Button } from 'reactstrap';
-class report extends Component {
+import React from 'react';
+import { Table, Button, Input, FormGroup, Form, Container, Row, Col, Label } from 'reactstrap';
+import locationBG from '../../img/BG_bl.jpg';
+
+export default class report extends React.Component {
     render() {
+        const divStyle = {
+            color: 'blue',
+            backgroundImage: 'url(' + locationBG + ')',
+        };
         return (
-            <div className="container">
-                <br /><br /><br /><br />
+            <div>
+                <section className="probootstrap-intro probootstrap-intro-inner" style={divStyle} data-stellar-background-ratio="0.5">
+                    <br /><br /><br /><br /><br /><br /><br /><br />
+                    <center>
+                        <h1>รายงาน</h1>
+                    </center>
 
-                <table>
-                    <tr>
-                        <td>
+                    <span className="probootstrap-animate">
+                        <a className="probootstrap-scroll-down js-next" href="#next-section">คลิกเลื่อน
+                        <i className="icon-chevron-down"></i></a></span>
+                </section>
+                <div>
+                    <section id="next-section" className="probootstrap-section">
+                        <Container>
+                            <FormGroup>
+                                <Row><Label for="exampleSelect">Total All Reports in system : value in Database</Label></Row>
+                                <Row align="center">
+                                    <Col>
+                                        <Input type="select" name="select">
+                                            <option>ประเภทรายงาน</option>
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                        </Input>
+                                    </Col>
+
+                                    <Col>
+                                        <Button color="success">ตกลง</Button>{' '}
+                                    </Col>
+                                </Row>
+                            </FormGroup>
                             <Form>
-                                <Input type="text" placeholder="Search"></Input>
+                                <Table bordered>
+                                    <thead>
+                                        <tr>
+                                            <th>ประเภท</th>
+                                            <th>รายงาน</th>
+                                            <th>ข้อมูลที่ตรวจวัด</th>
+                                            <th>โหลด</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Detail1</td>
+                                            <td>Detail1</td>
+                                            <td>Detail1</td>
+                                            <td><Button color="success">โหลด</Button>{' '}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Detail2</td>
+                                            <td>Detail2</td>
+                                            <td>Detail2</td>
+                                            <td><Button color="success">โหลด</Button>{' '}</td>
+                                        </tr>
+                                    </tbody>
+                                </Table>
                             </Form>
-
-                            <Input type="select">
-                                <option>Report type</option>
-                                </Input>
-
-                        </td>
-                        <td>
-                            &nbsp;&nbsp;
-            <Button color="success">Enter</Button>{' '}
-                        </td>
-                    </tr>
-                </table>
-
-
-
-
+                        </Container>
+                    </section>
+                </div>
             </div>
         );
     }
 }
-
-export default report;
