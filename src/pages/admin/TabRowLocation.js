603862a5db4a28d25590ae1834361e1c607898b9
SiFlAMeSE <flame_uz@hotmail.com>
2019-02-04 21:46:20 +0700

    แก้ไขฟอร์มใหม่

--- src/pages/admin/TabRowLocation.js
+++ src/pages/admin/TabRowLocation.js
@@ -76,8 +76,8 @@ class TabRowLocation extends Component {
                 </Container>
               </ModalBody>
               <ModalFooter>
-                <Button type="delete" color="info" onClick={this.toggle}>แก้ไขข้อมูล</Button>{' '}
-                <Button color="danger" onClick={this.toggle}>ลบข้อมูล</Button>
+                <Button color="info" onClick={this.toggle}>แก้ไขข้อมูล</Button>{' '}
+                <Button color="danger" type="delete" onClick={this.toggle}>ลบข้อมูล</Button>
                 <Button color="secondary" onClick={this.toggle}>ยกเลิก</Button>
               </ModalFooter>
             </form>
