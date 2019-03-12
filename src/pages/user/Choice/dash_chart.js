92f86b7ed1a2ecfec572ebe13adacc2120e6baec
SiFlAMeSE <flame_uz@hotmail.com>
2019-03-12 17:02:20 +0700

    update animation

---
+++ src/pages/user/Choice/dash_chart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class dash_chart extends Component {
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
