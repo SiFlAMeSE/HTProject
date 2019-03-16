2cee9cfe69a47cd022580fc159d4ff4873b8301e
SompotSE <s5802041620203@email.kmutnb.ac.th>
2019-03-16 23:29:16 +0700

    edit date

--- src/pages/user/Tabalhistory.js
+++ src/pages/user/Tabalhistory.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
 class Tablehistory extends Component {
   render() {
     return (
-        <tr>
-          <td>
-            {this.props.obj.t}
-          </td>
-          <td>
-            {this.props.obj.h}
-          </td>
-          <td>
-            {this.props.obj.mac}
-          </td>
-          <td>
-            {this.props.obj.date}
-          </td>
-        </tr>
+      <tr>
+        <td>
+          {this.props.obj.t}
+        </td>
+        <td>
+          {this.props.obj.h}
+        </td>
+        <td>
+          {this.props.obj.mac}
+        </td>
+        <td>
+          {moment(this.props.obj.date).format('MMMM Do YYYY, h:mm:ss a')}
+        </td>
+      </tr>
     );
   }
 }
