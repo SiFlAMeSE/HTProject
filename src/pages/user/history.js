e3e4e11d47f45408a0347d87bc0299771df858e9
SompotSE <s5802041620203@email.kmutnb.ac.th>
2019-03-28 12:54:35 +0700

    Edit history

--- src/pages/user/history.js
+++ src/pages/user/history.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { Table, Button, Input, Form, Container, Row, Col } from 'reactstrap';
 import axios from 'axios';
-import Tablehistory from './Tabalhistory';
+import ChartHistory from './Chart_history';
 import SenserChoice from './Choice/SenserChoice';
 import moment from 'moment';
 import { CSVLink } from "react-csv";
 
-var _mac, _dateP, mac, date, Build, Location;
-var bu_num, Loca_num;
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import 'react-day-picker/lib/style.css';
+import { formatDate, parseDate } from 'react-day-picker/moment';
+
+var _mac, _datePf, _datePt, mac, Build, Location, date_Next, dateS = [], History;
+var bu_num, Loca_num, date_num, his_num;
 var data_ss, csv_num = 0;
 const headers = [
     { label: "ID", key: "_id" },
@@ -17,6 +21,8 @@ const headers = [
     { label: "Date", key: "date" }
 ];
 const csvData = [];
+var status = 0;
+var show = [], show_num = 0;
 
 class history extends Component {
     constructor(props) {
@@ -26,24 +32,54 @@ class history extends Component {
             data: {}
         }
 
-        this.state = { History: [], Senser: [] };
+        this.handleFromChange = this.handleFromChange.bind(this);
+        this.handleToChange = this.handleToChange.bind(this);
+        this.state = {
+            from: undefined,
+            to: undefined,
+        };
+
+
+        this.state = { History: [], Senser: [], Data_Temp: [] };
 
     }
 
     componentWillMount() {
         _mac = this.props.match.params.id
-        _dateP = this.props.match.params.date
+        _datePf = this.props.match.params.datef
+        _datePt = this.props.match.params.datet
         // var test = moment('2019-02-18 08:37:51.968Z').format('YYYY-MM-DD')
         //console.log(test)
-        console.log(_mac)
-        console.log(_dateP)
+        // console.log(_mac)
+        // console.log(_datePf)
+        // console.log(_datePt)
+        //console.log(moment(_datePt).to(moment(_datePf), moment(_datePt)))
+        date_num = (moment(_datePt).diff(moment(_datePf), 'days')) + 1
+        date_Next = _datePf
+        for (let i = 0; i < date_num; i++) {
+            if (i == 0) {
+                dateS[i] = _datePf
+                //console.log(moment(_datePf).format('YYYY-MM-DD'))
+            }
+            else {
+                date_Next = moment(date_Next).add(1, 'days').format('YYYY-MM-DD')
+                dateS[i] = date_Next
+                //console.log(date_Next)
+            }
+
+        }
+        //console.log(dateS)
+        //console.log(_datePt.to(_datePf, _datePt))
+        status = 0
+        //console.log(status)
         data_ss = JSON.parse(sessionStorage.getItem('Login_add'))
     }
 
     componentDidMount() {
         axios.get('http://localhost:5000/history')
             .then(response => {
-                const History = response.data;
+                History = response.data;
+                his_num = response.data.length;
                 this.setState({ History: History });
                 //console.log(History)
             })
@@ -82,16 +118,18 @@ class history extends Component {
             .catch(function (error) {
                 console.log(error);
             })
+
+        this.setState({ Data_Temp: dateS });
     }
 
     onchangeMAC(e) {
         mac = e.target.value
     }
 
-    onchangeDate(e) {
-        date = e.target.value
-        //console.log(date)
-    }
+    // onchangeDate(e) {
+    //     date = e.target.value
+    //     //console.log(date)
+    // }
 
     // onSubmit(e) {
     //     window.location.replace('/history/' + this.state.mac)
@@ -99,53 +137,152 @@ class history extends Component {
     // }
 
     sentid = (e) => {
-        window.location.replace('/history/' + mac + '/' + date)
+        window.location.replace('/history/' + mac + '/' + moment(this.state.from).format('YYYY-MM-DD') + '/' + moment(this.state.to).format('YYYY-MM-DD'))
     }
 
     sentEx = (e) => {
         return <CSVLink data={csvData} headers={headers}><img width="180px" src={require('../../img/downloadEx.png')} /></CSVLink>
     }
 
-    tabRow() {
-        return this.state.History.map((object, i) => {
-            const _date = moment(object.date).format('YYYY-MM-DD')
-            var set = "undefined"
-            //console.log(set)
-            if (_mac !== set && _dateP === set) {
-                if (_mac === object.mac) {
-                    csvData[csv_num] = object
-                    csv_num = csv_num + 1
-                    return <Tablehistory obj={object} key={i} />;
-                } else
-                    return false
+    // tabRow() {
+    //     return this.state.History.map((object, i) => {
+    //         const _date = moment(object.date).format('YYYY-MM-DD')
+    //         var set = "undefined"
+    //         //console.log(set)
+    //         if (_mac !== set && _dateP === set) {
+    //             if (_mac === object.mac) {
+    //                 csvData[csv_num] = object
+    //                 csv_num = csv_num + 1
+    //                 return <Tablehistory obj={object} key={i} />;
+    //             } else
+    //                 return false
+    //         }
+    //         else if (_mac === set && _dateP !== set) {
+    //             if (_dateP === _date) {
+    //                 csvData[csv_num] = object
+    //                 csv_num = csv_num + 1
+    //                 return <Tablehistory obj={object} key={i} />;
+    //             } else
+    //                 return false
+    //         }
+    //         else if (_mac !== set && _dateP !== set) {
+    //             if (_mac === object.mac && _dateP === _date) {
+    //                 csvData[csv_num] = object
+    //                 csv_num = csv_num + 1
+    //                 return <Tablehistory obj={object} key={i} />;
+    //             } else
+    //                 return false
+    //         } else
+    //             return false
+    //     });
+    // }
+
+    Create2DArray(rows) {
+        var arr = [];
+
+        for (var i = 0; i < rows; i++) {
+            arr[i] = [];
+        }
+
+        return arr;
+    }
+
+    Select_Data() {
+        if (status == 0) {
+            for (let i = 0; i < date_num; i++) {
+                // console.log(his_num)
+                // console.log(History)
+                for (let z = 0; z < his_num; z++) {
+                    const _date = moment(History[z].date).format('YYYY-MM-DD')
+                    if (_mac === History[z].mac && _date === dateS[i]) {
+                        //console.log(History[z])
+                        csvData[csv_num] = History[z]
+                        csv_num = csv_num + 1
+                        //return <Tablehistory obj={History[z]} key={i} />;
+                        //console.log(test)
+                    }
+                }
+                //console.log(dateS[i])
             }
-            else if (_mac === set && _dateP !== set) {
-                if (_dateP === _date) {
-                    csvData[csv_num] = object
-                    csv_num = csv_num + 1
-                    return <Tablehistory obj={object} key={i} />;
-                } else
-                    return false
+
+            var temp_sum = 0, humdi_sum = 0, temp_num = 0, humdi_num = 0
+            var data_temp = this.Create2DArray(50), data_humdi = this.Create2DArray(50)
+            // data_temp = [[]]
+            // data_humdi = [[]]
+            for (let x = 0; x < date_num; x++) {
+                //console.log(x)
+                for (var a = 0; a < 24; a++) {
+                    temp_sum = 0
+                    humdi_sum = 0
+                    temp_num = 0
+                    humdi_num = 0
+                    for (let y = 0; y < csv_num; y++) {
+                        const _date = moment(csvData[y].date).format('YYYY-MM-DD')
+                        const _hous = moment(csvData[y].date).format('HH')
+                        if (_date === dateS[x] && parseInt(_hous) === a) {
+                            temp_sum = temp_sum + csvData[y].t
+                            temp_num = temp_num + 1
+                            humdi_sum = humdi_sum + csvData[y].h
+                            humdi_num = humdi_num + 1
+                            // console.log(parseInt(_hous))
+                            // console.log(a)
+                        }
+                    }
+                    data_temp[x][a] = (temp_sum / temp_num)
+                    data_humdi[x][a] = (humdi_sum / humdi_num)
+
+                }
+                // console.log(data_temp)
+                // console.log(data_humdi)
+
+                //this.dataGraph(data_temp, data_humdi)
             }
-            else if (_mac !== set && _dateP !== set) {
-                if (_mac === object.mac && _dateP === _date) {
-                    csvData[csv_num] = object
-                    csv_num = csv_num + 1
-                    return <Tablehistory obj={object} key={i} />;
-                } else
-                    return false
-            } else
-                return false
-        });
+
+            for (let x = 0; x < date_num; x++) {
+                for (let i = 0; i < 24; i++) {
+                    //console.log(this.props.obj1[i].toString())
+                    if (data_temp[x][i].toString() === "NaN") {
+                        data_temp[x][i] = "0"
+                        data_humdi[x][i] = "0"
+                    }
+                    else {
+                        data_temp[x][i] = data_temp[x][i].toString()
+                        data_humdi[x][i] = data_humdi[x][i].toString()
+                    }
+                }
+            }
+
+            // console.log(data_temp)
+            // console.log(data_humdi)
+            this.showData()
+
+            return this.state.Data_Temp.map(function (object, i) {
+                return <ChartHistory obj1={data_temp[i]} obj2={data_humdi[i]} obj3={dateS[i]} />
+            });
+
+        }
     }
 
     choice() {
+        show = [];
+        show_num = 0;
         return this.state.Senser.map(function (object, i) {
             for (let z = 0; z < bu_num; z++) {
                 if (object.Id_Build === Build[z]._id) {
                     for (let y = 0; y < Loca_num; y++) {
                         if (Build[z].Id_Loca === Location[y]._id) {
                             if (data_ss._id === Location[y].Id_Admin) {
+                                show[show_num] = {
+                                    Name_Lo: Location[y].Name_Lo,
+                                    Name_Build: Build[z].Name_Build,
+                                    Position: object.Position,
+                                    Temp_Low: object.Temp_Low,
+                                    Temp_Hight: object.Temp_Hight,
+                                    Humdi_Low: object.Humdi_Low,
+                                    Humdi_Hight: object.Humdi_Hight,
+                                    Macaddress: object.Macaddress
+                                }
+                                show_num = show_num + 1
                                 return <SenserChoice obj={object} key={i} />;
                             }
                         }
@@ -156,9 +293,88 @@ class history extends Component {
         });
     }
 
+    showData() {
+        // console.log(show)
+        // console.log(show_num)
+        if (_mac !== "undefined") {
+            console.log("No deil")
+            for (let i = 0; i < show_num; i++) {
+                if (_mac === show[i].Macaddress) {
+                    return <Table >
+                        <Row align="center">
+                            <Col>
+                                <div>ข้อมูลตั้งค่า</div>
+                            </Col>
+                            <Col>
+                                <div>ข้อมูลเซนเซอร์ </div>
+                            </Col>
+                        </Row>
+                        <Row align="center">
+                            <Col>
+                                <div>สถานที่ : {show[i].Name_Lo}</div>
+                            </Col>
+                            <Col>
+                                <div>อุณหภูมิสูงสุด : {show[i].Temp_Hight} </div>
+                            </Col>
+                        </Row>
+                        <Row align="center">
+                            <Col>
+                                <div>อาคาร : {show[i].Name_Build}</div>
+                            </Col>
+                            <Col>
+                                <div>อุณหภูมิตำสุด : {show[i].Temp_Low} </div>
+                            </Col>
+                        </Row>
+                        <Row align="center">
+                            <Col>
+                                <div>ห้องติดตั้งเซนเซอร์ : {show[i].Position}</div>
+                            </Col>
+                            <Col>
+                                <div>ความชื้นสูงสุด : {show[i].Humdi_Hight} </div>
+                            </Col>
+                        </Row>
+                        <Row align="center">
+                            <Col>
+                                <div>รหัสเครื่อง : {show[i].Macaddress}</div>
+                            </Col>
+                            <Col>
+                                <div>ความชื้นต่ำสุด : {show[i].Humdi_Low} </div>
+                            </Col>
+                        </Row>
+                    </Table>
+                }
+            }
+        }
+    }
+
+    // ปฏิทิน
+    showFromMonth() {
+        const { from, to } = this.state;
+        if (!from) {
+            return;
+        }
+        if (moment(to).diff(moment(from), 'months') < 2) {
+            this.to.getDayPicker().showMonth(from);
+        }
+    }
+    handleFromChange(from) {
+        // Change the from date and focus the "to" input field
+        this.setState({ from });
+        status = 1
+        //console.log(status)
+    }
+    handleToChange(to) {
+        this.setState({ to }, this.showFromMonth);
+    }
+
     render() {
-        console.log(csv_num);
-        console.log(csvData);
+        // console.log(csv_num);
+        // console.log(csvData);
+        // console.log(moment(this.state.to).to(moment(this.state.from), moment(this.state.to)))
+        // console.log(data_temp[0][0])
+        // console.log(data_humdi[0][0])
+        const { from, to } = this.state;
+        //const modifiers = { start: from, end: to };
         return (
             <div>
                 <div>
@@ -169,10 +385,10 @@ class history extends Component {
                     </div>
                         </div>
                     </section>
-                    <Container> 
+                    <Container>
                         <Form>
                             <Row>
-                                <Col style={{ paddingTop: '25px',paddingLeft: '70px' }}>
+                                <Col style={{ paddingTop: '25px', paddingLeft: '70px' }}>
                                     <Input type="select" name="select" onChange={this.onchangeMAC}>
                                         <option value="undefined">เลือกเซนเซอร์</option>
                                         {this.choice()}
@@ -180,13 +396,51 @@ class history extends Component {
                                 </Col>
 
                                 <Col style={{ paddingTop: '25px' }}>
-                                    <Input
+                                    <span className="InputFromTo">
+                                        <DayPickerInput
+                                            value={from}
+                                            placeholder="From"
+                                            // format="LL"
+                                            formatDate={formatDate}
+                                            parseDate={parseDate}
+                                            dayPickerProps={{
+                                                selectedDays: [from, { from, to }],
+                                                disabledDays: { after: to },
+                                                toMonth: to,
+                                                modifiers: { start: from, end: to },
+                                                numberOfMonths: 2,
+                                                onDayClick: () => this.to.getInput().focus(),
+                                            }}
+                                            onDayChange={this.handleFromChange}
+                                        />{' '}
+                                        —{' '}
+                                        <span className="InputFromTo-to">
+                                            <DayPickerInput
+                                                ref={el => (this.to = el)}
+                                                value={to}
+                                                placeholder="To"
+                                                // format="LL"
+                                                formatDate={formatDate}
+                                                parseDate={parseDate}
+                                                dayPickerProps={{
+                                                    selectedDays: [from, { from, to }],
+                                                    disabledDays: { before: from },
+                                                    modifiers: { start: from, end: to },
+                                                    month: from,
+                                                    fromMonth: from,
+                                                    numberOfMonths: 2,
+                                                }}
+                                                onDayChange={this.handleToChange}
+                                            />
+                                        </span>
+                                    </span>
+                                    {/* <Input
                                         type="date"
                                         name="date"
                                         id="exampleDate"
                                         placeholder="ระบุวันเดือนปี"
                                         onChange={this.onchangeDate}
-                                    />
+                                    /> */}
                                 </Col>
                                 <Col>
                                     <Button color="primary" onClick={(e) => this.sentid(e)}>ค้นหา</Button>
@@ -194,27 +448,13 @@ class history extends Component {
                                 </Col>
 
                             </Row>
-
-
-
                         </Form>
-                        <Form id="spacetop">
-                            <Table bordered>
-                                <thead>
-                                    <tr>
-                                        <td>อุณหภูมิ</td>
-                                        <td>ความชื้น</td>
-                                        <td>รหัสเครื่อง</td>
-                                        <td>วันที่และเวลา</td>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {this.tabRow()}
-                                </tbody>
-                            </Table>
-                        </Form>
+                        {this.showData()}
                     </Container>
-
+                    
+                    <Form id="spacetop">
+                        {this.Select_Data()}
+                    </Form>
                 </div>
             </div >
         );
--- src/testcode.js
+++ src/testcode.js
@@ -1,113 +1,105 @@
 import React from 'react';
-import axios from 'axios';
-import Show from './Show';
-import { Form } from 'reactstrap';
+import moment from 'moment';
+import Helmet from 'react-helmet';
 
-var users, data_user;
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import 'react-day-picker/lib/style.css';
+
+import { formatDate, parseDate } from 'react-day-picker/moment';
+
 export default class testcode extends React.Component {
   constructor(props) {
     super(props);
-
+    this.handleFromChange = this.handleFromChange.bind(this);
+    this.handleToChange = this.handleToChange.bind(this);
     this.state = {
-      data: {},
+      from: undefined,
+      to: undefined,
+    };
+  }
+  showFromMonth() {
+    const { from, to } = this.state;
+    if (!from) {
+      return;
     }
-
-    this.state = { users: [] };
-  }
-
-  componentWillMount() {
-    // data_user = this.props.obj.length-1
-  }
-
-  componentDidMount() {
-    axios.get('http://localhost:5000/users/user_list')
-      .then(res => {
-        users = res.data;
-        this.setState({ users });
-        console.log(users.length)
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (this.state.users.length !== nextProps.users.length) {
-      this.showtest();
+    if (moment(to).diff(moment(from), 'months') < 2) {
+      this.to.getDayPicker().showMonth(from);
     }
   }
-
-  showtest() {
-    // this.forceUpdate();
-    return this.state.users.map((object, i) => {
-      return <Show obj={object} key={i} />
-    })
+  handleFromChange(from) {
+    // Change the from date and focus the "to" input field
+    this.setState({ from });
   }
-
-  // componentWillReceiveProps(nextProps){
-  //   this.showtest(nextProps)
-  // }
-
-  // componentWillReceiveProps(nextProps){
-  //   axios.get('http://localhost:5000/users/user_list')
-  //   .then(res => {
-  //     users = res.data;
-  //     this.setState({ users });
-  //     console.log(users)
-  //   })
-  //   .catch(function(error) {
-  //     console.log(error);
-  //   })
-  // }
-
+  handleToChange(to) {
+    this.setState({ to }, this.showFromMonth);
+  }
   render() {
+    const { from, to } = this.state;
+    const modifiers = { start: from, end: to };
     return (
-      <div>{this.state.users.length}
-        <br /><br />
-        {this.showtest()}
-        <br /><br />
-        <form>
-          <label>
-            ประเภทการแจ้งเตือน
-          <div>
-              <input
-                name="error"
-                type="checkbox"
-                value="error"
-                onChange={this.handleInputChange} />พบข้อบกพร่อง
-              <span style={{ paddingRight: '15px' }} />
-              <input
-                name="max_t"
-                type="checkbox"
-                value="max_t"
-                onChange={this.handleInputChange} />อุณหภูมิสูงกว่ากำหนด
-              <span style={{ paddingRight: '15px' }} />
-              <input
-                name="min_t"
-                type="checkbox"
-                value="min_t"
-                onChange={this.handleInputChange} />อุณหภูมิต่ำกว่ากำหนด
-              <span style={{ paddingRight: '15px' }} />
-              <input
-                name="max_h"
-                type="checkbox"
-                value="max_h"
-                onChange={this.handleInputChange} />ความชื้นสูงกว่ากำหนด
-              <span style={{ paddingRight: '15px' }} />
-              <input
-                name="min_h"
-                type="checkbox"
-                value="min_h"
-                onChange={this.handleInputChange} />ความชื้นต่ำกว่ากำหนด
-          </div>
-          </label>
-          <br />
-        </form>
+      <div className="InputFromTo">
+        <DayPickerInput
+          value={from}
+          placeholder="From"
+          // format="LL"
+          formatDate={formatDate}
+          parseDate={parseDate}
+          dayPickerProps={{
+            selectedDays: [from, { from, to }],
+            disabledDays: { after: to },
+            toMonth: to,
+            modifiers,
+            numberOfMonths: 2,
+            onDayClick: () => this.to.getInput().focus(),
+          }}
+          onDayChange={this.handleFromChange}
+        />{' '}
+        —{' '}
+        <span className="InputFromTo-to">
+          <DayPickerInput
+            ref={el => (this.to = el)}
+            value={to}
+            placeholder="To"
+            // format="LL"
+            formatDate={formatDate}
+            parseDate={parseDate}
+            dayPickerProps={{
+              selectedDays: [from, { from, to }],
+              disabledDays: { before: from },
+              modifiers,
+              month: from,
+              fromMonth: from,
+              numberOfMonths: 2,
+            }}
+            onDayChange={this.handleToChange}
+          />
+        </span>
+        {/* <Helmet>
+          <style>{`
+  .InputFromTo .DayPicker-Day--selected:not(.DayPicker-Day--start):not(.DayPicker-Day--end):not(.DayPicker-Day--outside) {
+    background-color: #f0f8ff !important;
+    color: #4a90e2;
+  }
+  .InputFromTo .DayPicker-Day {
+    border-radius: 0 !important;
+  }
+  .InputFromTo .DayPicker-Day--start {
+    border-top-left-radius: 50% !important;
+    border-bottom-left-radius: 50% !important;
+  }
+  .InputFromTo .DayPicker-Day--end {
+    border-top-right-radius: 50% !important;
+    border-bottom-right-radius: 50% !important;
+  }
+  .InputFromTo .DayPickerInput-Overlay {
+    width: 550px;
+  }
+  .InputFromTo-to .DayPickerInput-Overlay {
+    margin-left: -198px;
+  }
+`}</style>
+        </Helmet> */}
       </div>
-
-
-
-
     );
   }
 }
