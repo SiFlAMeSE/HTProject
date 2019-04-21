fc5d4451b08ab7e2d222923d86e63d903881b5b0
SompotSE <s5802041620203@email.kmutnb.ac.th>
2019-04-21 13:54:19 +0700

    Edit Pm

--- src/pages/user/ChartPM.js
+++ src/pages/user/ChartPM.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
 import { Line } from 'react-chartjs-2';
-var dataPM = []
+import moment from 'moment';
+
+var label_X = []
+var label_PM1 = 'PM1.0'
+var label_PM2 = 'PM2.5'
+var label_PM4 = 'PM4.0'
+var label_PM10 = 'PM10.0'
+var label_NC0 = 'NC0.5'
+var label_NC1 = 'NC1.0'
+var label_NC2 = 'NC2.5'
+var label_NC4 = 'NC4.0'
+var label_NC10 = 'NC10.0'
+var data_PM1 = []
+var data_PM2 = []
+var data_PM4 = []
+var data_PM10 = []
+var data_NC0 = []
+var data_NC1 = []
+var data_NC2 = []
+var data_NC4 = []
+var data_NC10 = []
+
 var data_set, data_num;
 
 class ChartPM extends Component {
@@ -10,16 +31,61 @@ class ChartPM extends Component {
 
         this.state = {
             chartData: {
-                labels: ['PM1.0', 'PM2.5', 'PM4.0', 'PM10.0', 'NC0.5', 'NC1.0', 'NC2.5', 'NC4.0', 'NC10.0'],
+                labels: label_X,
                 datasets: [
+                    // {
+                    //     label: 'แสดงการวัดค่าฝุน',
+                    //     backgroundColor: 'rgba(255,99,132,0.2)',
+                    //     borderColor: 'rgba(255,99,132,1)',
+                    //     borderWidth: 1,
+                    //     hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                    //     hoverBorderColor: 'rgba(255,99,132,1)',
+                    //     data: dataPM
+                    // }
                     {
-                        label: 'แสดงการวัดค่าฝุน',
-                        backgroundColor: 'rgba(255,99,132,0.2)',
-                        borderColor: 'rgba(255,99,132,1)',
-                        borderWidth: 1,
-                        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                        hoverBorderColor: 'rgba(255,99,132,1)',
-                        data: dataPM
+                        label: label_PM1,
+                        data: data_PM1,
+                        borderColor: ['rgba(255, 99, 132, 1)',]
+                    },
+                    {
+                        label: label_PM2,
+                        data: data_PM2,
+                        borderColor: ['rgba(255, 206, 86, 1)']
+                    },
+                    {
+                        label: label_PM4,
+                        data: data_PM4,
+                        borderColor: ['rgba(54, 162, 235, 1)',]
+                    },
+                    {
+                        label: label_PM10,
+                        data: data_PM10,
+                        borderColor: ['rgba(75, 192, 192, 1)']
+                    },
+                    {
+                        label: label_NC0,
+                        data: data_NC0,
+                        borderColor: ['rgba(153, 102, 255, 1)',]
+                    },
+                    {
+                        label: label_NC1,
+                        data: data_NC1,
+                        borderColor: ['rgba(255, 159, 64, 1']
+                    },
+                    {
+                        label: label_NC2,
+                        data: data_NC2,
+                        borderColor: ['rgba(255, 206, 86, 1)',]
+                    },
+                    {
+                        label: label_NC4,
+                        data: data_NC4,
+                        borderColor: ['rgba(255, 99, 132, 1)']
+                    },
+                    {
+                        label: label_NC10,
+                        data: data_NC10,
+                        borderColor: ['rgba(54, 162, 235, 1)',]
                     }
                 ]
             }
@@ -29,25 +95,22 @@ class ChartPM extends Component {
     componentWillMount() {
         data_set = this.props.obj
         data_num = this.props.obj.length - 1
-        //console.log(data_set)
-        //console.log(data_num)
-        // data_ss = JSON.parse(sessionStorage.getItem('Login_add'))
         JSON.parse(sessionStorage.getItem('Login_add'))
-
-        // console.log(data_set)
     }
 
     dataGraph() {
-        dataPM[0] = data_set[data_num].pm1
-        dataPM[1] = data_set[data_num].pm2
-        dataPM[2] = data_set[data_num].pm4
-        dataPM[3] = data_set[data_num].pm10
-        dataPM[4] = data_set[data_num].nc0
-        dataPM[5] = data_set[data_num].nc1
-        dataPM[6] = data_set[data_num].nc2
-        dataPM[7] = data_set[data_num].nc4
-        dataPM[8] = data_set[data_num].nc10
-        //console.log(dataPM)
+        for (let i = 19; i >= 0; i-- , data_num--) {
+            label_X[i] = moment(data_set[data_num].date).format('LTS')
+            data_PM1[i]  = data_set[data_num].pm1
+            data_PM2[i]  = data_set[data_num].pm2
+            data_PM4[i]  = data_set[data_num].pm4
+            data_PM10[i] = data_set[data_num].pm10
+            data_NC0[i]  = data_set[data_num].nc0
+            data_NC1[i]  = data_set[data_num].nc1
+            data_NC2[i]  = data_set[data_num].nc2
+            data_NC4[i]  = data_set[data_num].nc4
+            data_NC10[i] = data_set[data_num].nc10
+        }
     }
 
     render() {
@@ -74,8 +137,8 @@ class ChartPM extends Component {
                         scales: {
                             yAxes: [{
                                 ticks: {
-                                    max: 100,
-                                    min: -60,
+                                    max: 200,
+                                    min: 0,
                                     stepSize: 20
                                 }
                             }]
